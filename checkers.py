998dfdc71747104282c0ca5e2e830ca273660ffd
Aditya Baradwaj <abaradwaj@berkeley.edu>
2018-10-12 17:59:36 -0700

    CLI improvement

--- checkers.py
+++ checkers.py
@@ -146,7 +146,7 @@ class CheckersGame:
     
     def print_board(self):
         # Code for printing borrowed from https://stackoverflow.com/questions/13214809/pretty-print-2d-python-list
-        s = [[str(e) for e in row] for row in self.board]
+        s = [[str(e) if e is not None else '-' for e in row] for row in self.board]
         lens = [max(map(len, col)) for col in zip(*s)]
         fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
         table = [fmt.format(*row) for row in s]
